541c22b5ef96560e5f1c18c4384ce339960c2fd4
Jun Lu <locky1218@gmail.com>
2016-08-09 17:02:13 +0200

    update matrix factorization function

--- Recommender.py
+++ Recommender.py
@@ -50,22 +50,72 @@ def data_preprocessing():
 
     return RTe, R
 
+def max_factorize_ALS(X, hid_dim, lamb=0.08, max_iter=100, isSparse=False, Criterion=1e-5):
+    num_row, num_col = X.shape
+    if isSparse:
+        ## get all the non zeros rows and columns
+        idxRow = [np.nonzero(X[k])[0] for k in range(num_row)]
+        idxColumn = [np.nonzero(X[:, k])[0] for k in range(num_col)]
+
+        ## get the nonzero indexes
+        idxTr = np.nonzero(X)
+        numTotalTr = len(idxTr[0])  # numbers of nonzero entries in train datas
+    else:
+        ## get all the non zeros rows and columns
+        idxRow = [range(len(X[k])) for k in range(num_row)]
+        idxColumn = [range(len(X[:, k])) for k in range(num_col)]
+
+        ## get all the indexes
+        idxTr = np.where((X==0) | (X!=0))
+        numTotalTr = len(idxTr[0])  # numbers of nonzero entries in train datas
+
+    hid_dim += 2 # add two columns for the user and moview biases
+    P = np.random.rand(num_row, hid_dim)
+    Q = np.random.rand(num_col, hid_dim)
+    P[:, hid_dim-1] = np.ones(num_row) # assume the first column is the bias of each user
+    Q[:, 0] = np.ones(num_col)  # assum the last column is the bias of each film
+    L = np.zeros(max_iter)
+
+    # non-negative
+    # P = abs(P)
+    # Q = abs(Q)
+
+    for k in range(max_iter):
+        # ALS update
+        for m in range(M):
+            idx = idxColumn[m] # indexes of nonzero entries of movie m
+            nm = len(idx)
+            pm = P[idx, 1:hid_dim]
+            rm = X[idx, m]
+            tmp = np.linalg.inv(pm.T.dot(pm) + lamb*nm*np.eye(hid_dim-1))
+            Q[m, 1:hid_dim] = (tmp.dot(pm.T).dot(rm))  # do not update the first column, it's always 1's for the bias of each user
+            # non-negative
+            # Q[m, 1:F] = Q[m, 1:F]*(Q[m, 1:F] >= 0)
+        for u in range(U):
+            idx = idxRow[u] # indexes of nonzero entries of user u
+            nu = len(idx)
+            qu = Q[idx, 0:hid_dim-1]
+            ru = X[u, idx]
+            tmp = np.linalg.inv(qu.T.dot(qu) + lamb*nu*np.eye(hid_dim-1))
+            P[u, 0:hid_dim-1] = (tmp.dot(qu.T).dot(ru)) # do not update the las column, it's always 1's for the bias of each film
+            # non-negative
+            # P[u, 0:F-1] = P[u, 0:F-1]*(P[u, 0:F-1] >= 0)
+        R_pred = P.dot(Q.T)
+        R_pred = R_pred + np.sum(X[idxTr]-R_pred[idxTr])/numTotalTr
+        L[k] = np.sum((R[idxTr]-R_pred[idxTr])**2)/numTotalTr
+        print(('iteration %i: Train error=%.15f') %(k, L[k]))
+        # if k > 0 and L[k-1] - L[k] < Criterion:
+        #     break
+    return  P, Q, R_pred
+
 
 def test_recommender():
-    R_test, R_Train = data_preprocessing()
+    R_test, R_Train = data_preprocessing() # shape=(num_user, num_movie)
 
     idxTr = np.nonzero(R_Train)
-    numTotalTr = len(idxTr[0]) # numbers of nonzero entries in train datas
+    numTotalTr = len(idxTr[0])  # numbers of nonzero entries in test datas
     idxTe = np.nonzero(R_test)
     numTotalTe = len(idxTe[0]) # numbers of nonzero entries in test datas
-
-    # save the indices of zero elements
-    idxRow = [np.nonzero(R_Train[k])[0] for k in range(U)]
-    idxColumn = [np.nonzero(R_Train[:, k])[0] for k in range(M)]
-
-
-    # take log
-    # R[idxTr] = np.log(R[idxTr])
 
     # initialize
     lamb = 0.08  # the value of lambda can be set by cross validation
@@ -73,48 +123,15 @@ def test_recommender():
     maxIter = 30 # try different iteration
     Criterion = 1e-5
 
-    F += 2 # add two columns for the user and moview biases
-    P = np.random.rand(U, F)
-    Q = np.random.rand(M, F)
-    P[:, F-1] = np.ones(U) # assume the first column is the bias of each user
-    Q[:, 0] = np.ones(M)  # assum the last column is the bias of each film
-    L = np.zeros(maxIter)
-
-    # non-negative
-    # P = abs(P)
-    # Q = abs(Q)
-
-    for k in range(maxIter):
-        # ALS update
-        for m in range(M):
-            idx = idxColumn[m] # indexes of nonzero entries of movie m
-            nm = len(idx)
-            pm = P[idx, 1:F]
-            rm = R_Train[idx, m]
-            tmp = np.linalg.inv(pm.T.dot(pm) + lamb*nm*np.eye(F-1))
-            Q[m, 1:F] = (tmp.dot(pm.T).dot(rm))  # do not update the first column, it's always 1's for the bias of each user
-            # non-negative
-            # Q[m, 1:F] = Q[m, 1:F]*(Q[m, 1:F] >= 0)
-        for u in range(U):
-            idx = idxRow[u] # indexes of nonzero entries of user u
-            nu = len(idx)
-            qu = Q[idx, 0:F-1]
-            ru = R_Train[u, idx]
-            tmp = np.linalg.inv(qu.T.dot(qu) + lamb*nu*np.eye(F-1))
-            P[u, 0:F-1] = (tmp.dot(qu.T).dot(ru)) # do not update the las column, it's always 1's for the bias of each film
-            # non-negative
-            # P[u, 0:F-1] = P[u, 0:F-1]*(P[u, 0:F-1] >= 0)
-        R_pred = P.dot(Q.T)
-        R_pred = R_pred + np.sum(R_Train[idxTr]-R_pred[idxTr])/numTotalTr
-        L[k] = np.sum((R[idxTr]-R_pred[idxTr])**2)/numTotalTr
-        print(('iteration %i: Train error=%.15f') %(k, L[k]))
-        if k > 0 and L[k-1] - L[k] < Criterion:
-            break
-
-    # R_pred = np.exp(R_pred)
+    ## Matrix factorization
+    P, Q, R_pred = max_factorize_ALS(R_Train, F, lamb=lamb, max_iter=maxIter, isSparse=True, Criterion=Criterion)
+    R_pred = P.dot(Q.T)
 
     R_pred[np.where(R_pred > 5)] = 5
     R_pred[np.where(R_pred < 0)] = 0
+
+    RMSETr = np.sum((R_Train[idxTr]-R_pred[idxTr])**2) / numTotalTr
+    print('Train error', RMSETr)
 
     RMSETe = np.sum((R_test[idxTe]-R_pred[idxTe])**2) / numTotalTe
     print('Test error', RMSETe)
